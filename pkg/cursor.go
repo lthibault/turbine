091f703c2aabbf3d1c62b534465b35f1f9562e35
Louis Thibault <l.thibault@sentimens.com>
2019-03-25 21:54:17 +0000

    Cursor requires CAS during init

--- pkg/cursor.go
+++ pkg/cursor.go
@@ -1,5 +1,7 @@
 package turbine
 
+import "sync/atomic"
+
 type cursor struct {
 	idx     int64
 	padding [7]int64 // cache-line padding
@@ -7,9 +9,9 @@ type cursor struct {
 
 func newCursor() *cursor { return &cursor{idx: -1} }
 
-func (c cursor) Load() int64 { return c.idx }
+func (c *cursor) Load() int64 { return atomic.LoadInt64(&c.idx) }
 
-func (c *cursor) Store(i int64) { c.idx = i }
+func (c *cursor) Store(i int64) { atomic.StoreInt64(&c.idx, i) }
 
 type barrier interface {
 	Load() int64
