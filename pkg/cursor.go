523dd3d9b93835b56e22b16c01e4e80df533f652
Louis Thibault <l.thibault@sentimens.com>
2019-03-25 00:31:46 +0000

    Don't use CAS for cursor

--- pkg/cursor.go
+++ pkg/cursor.go
@@ -1,7 +1,5 @@
 package turbine
 
-import "sync/atomic"
-
 type cursor struct {
 	idx     int64
 	padding [7]int64 // cache-line padding
@@ -9,9 +7,9 @@ type cursor struct {
 
 func newCursor() *cursor { return &cursor{idx: -1} }
 
-func (c *cursor) Load() int64 { return atomic.LoadInt64(&c.idx) }
+func (c cursor) Load() int64 { return c.idx }
 
-func (c *cursor) Store(i int64) { atomic.StoreInt64(&c.idx, i) }
+func (c *cursor) Store(i int64) { c.idx = i }
 
 type barrier interface {
 	Load() int64
